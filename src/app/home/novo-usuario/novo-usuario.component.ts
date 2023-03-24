d16f7eefbcb211cac9305c986550800092168cc7
Tiago Souza Garcia <garciats@saude.sc.gov.br>
2023-03-24 17:51:48 -0300

    validacoes

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { UsuarioExisteService } from './usuario-existe.service';
+import { NovoUsuarioService } from './novo-usuario.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+import {
+  minusculoValidator,
+  validadorSenha,
+} from './validador-personalizado.validator';
 
 @Component({
   selector: 'app-novo-usuario',
   templateUrl: './novo-usuario.component.html',
-  styleUrls: ['./novo-usuario.component.css']
+  styleUrls: ['./novo-usuario.component.css'],
 })
-export class NovoUsuarioComponent {
+export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService,
+    private usuarioExistenteService: UsuarioExisteService
+  ) {}
+
+  ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
+      userName: [
+        '',
+        [minusculoValidator],
+        [this.usuarioExistenteService.usuarioJaExiste()],
+      ],
+      password: [
+        '',
+        [Validators.required, Validators.minLength, validadorSenha()],
+      ],
+    });
+  }
+
+  cadastrar() {
+    console.log('Funcionando');
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+  }
 }
---
+++ src/app/home/novo-usuario/validador-personalizado.validator.ts
@@ -0,0 +1,30 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function validadorSenha(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const senha = control.value;
+
+    if (!senha) {
+      return null;
+    }
+
+    const temMaiusculo = /[A-Z]+/.test(senha);
+
+    const temMinusculo = /[a-z]+/.test(senha);
+
+    const temNumeral = /[0-9]+/.test(senha);
+
+    const senhaValida = temMaiusculo && temMinusculo && temNumeral;
+
+    return !senhaValida ? { senhaForte: true } : null;
+  };
+}
+
+export function minusculoValidator(control: AbstractControl) {
+  const valor = control.value as string;
+  if (valor !== valor.toLowerCase()) {
+    return { minusculo: true };
+  } else {
+    return null;
+  }
+}
